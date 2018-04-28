1089f35f5d5fbb4205cb78b59cc43c7797a7e2dc
Anton Patsev <patsev.anton@gmail.com>
2018-04-28 20:34:55 +0600

    Added check port puppet master

--- install_puppetdb_server.sh
+++ install_puppetdb_server.sh
@@ -3,6 +3,12 @@
 read -p "Enter FQDN Puppet Master: " fqdn_puppet_master
 read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
+</dev/tcp/$fqdn_puppet_master/8140
+if [ "$?" -ne 0 ]; then
+  echo "Connection to $fqdn_puppet_master on port 8140 failed"
+  exit 1
+fi
+
 yum install -y https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum install -y puppet-agent ntp
 echo "[main]" > /etc/puppetlabs/puppet/puppet.conf
