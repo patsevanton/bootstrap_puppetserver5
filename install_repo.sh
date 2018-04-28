693e8cd009fcf5054d1b633d884db9f8aaee6bf2
Anton Patsev <patsev.anton@gmail.com>
2018-04-28 13:11:14 +0600

    Update install_repo.sh

--- install_repo.sh
+++ install_repo.sh
@@ -14,8 +14,12 @@ yum install -y https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 #Install puppetserver
 yum install -y puppetserver ntp
 
+#Get total memory using free
 mem=$(free -m|awk '/^Mem:/{print $2}')
+#Get memory/2
 mem_devide_2=$(( $mem / 2 ))
+#Get memory/4
 mem_devide_4=$(( $mem / 4 ))
 
-sed -i "s/-Xms2g -Xmx2g/-Xms$mem_devide_2 -Xmx$mem_devide_2 -XX:MaxPermSize=$mem_devide_4/" /etc/sysconfig/puppetserver
+sed -i "s/-Xms2g -Xmx2g/-Xms$mem_devide_2 -Xmx$mem_devide_2/" /etc/sysconfig/puppetserver
+sed -i "s/MaxPermSize=256m/MaxPermSize=$mem_devide_4/" /etc/sysconfig/puppetserver
