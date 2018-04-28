ed366b026c68adafbe1b86a300d45336048a0bb2
Anton Patsev <patsev.anton@gmail.com>
2018-04-28 17:40:35 +0600

    Update install_puppetdb_server.sh

--- install_puppetdb_server.sh
+++ install_puppetdb_server.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+read -p "Enter FQDN Puppet Master: " fqdn_puppet_master
+read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
 yum install -y https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum install -y puppet-agent ntp
 echo "[main]" >> /etc/puppetlabs/puppet/puppet.conf
-echo "server = test-tools-puppet-apatsev1.russianpost.ru" >> /etc/puppetlabs/puppet/puppet.conf
-echo "ca_server = test-tools-puppet-apatsev1.russianpost.ru" >> /etc/puppetlabs/puppet/puppet.conf
+echo "server = $fqdn_puppet_master" >> /etc/puppetlabs/puppet/puppet.conf
+echo "ca_server = $fqdn_puppet_master" >> /etc/puppetlabs/puppet/puppet.conf
