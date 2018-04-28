9131c17bb63f5d6030f6402655e1a586421f0db6
Anton Patsev <patsev.anton@gmail.com>
2018-04-28 17:34:02 +0600

    Update install_puppetdb_server.sh

--- install_puppetdb_server.sh
+++ install_puppetdb_server.sh
@@ -2,3 +2,6 @@
 
 yum install -y https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum install -y puppet-agent ntp
+echo "[main]" >> /etc/puppetlabs/puppet/puppet.conf
+echo "server = test-tools-puppet-apatsev1.russianpost.ru" >> /etc/puppetlabs/puppet/puppet.conf
+echo "ca_server = test-tools-puppet-apatsev1.russianpost.ru" >> /etc/puppetlabs/puppet/puppet.conf
